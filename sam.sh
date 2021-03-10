5171e86d957ccbfb72e4d0b52cffda92e5092622
Samuel França <samuelffn@gmail.com>
2021-03-10 17:25:54 -0300

    Add scripts

---
+++ sam.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#linha=SERVICE.ATTENDANCES.COUNTER_TIP.ON_HOLD
+
+cd ~/dev/workspace/qless/qless-asc/code/client/admin
+
+grep -r -l -w $1
+
+if [ ! $? -eq 0 ]
+then
+  cd ~/scripts
+  echo $1>>result.txt
+fi 
+
+#!/bin/bash
+# ARQUIVO: list
+#
+#numLinha=1
+#while read linha
+#do
+#  echo "$numLinha $linha"
+#  numLinha=`expr $numLinha + 1`
+#done < arq.txt
---
+++ test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#arquivo=$1
+#dir=~/dev/workspace/qless/qless-asc/code/client/admin
+tatu=SERVICE.ATTENDANCES.COUNTER_TIP.ON_HOLD
+arr=(carro)
+#declare -A arr
+
+if [ -f result.txt ] 
+then
+  rm result.txt
+fi
+
+search(){
+#while IFS= read -r linha || [[ -n "$linha" ]]
+while read linha
+#for linha in $(cat arq.txt)
+do
+	#bash sam.sh $linha
+	#cd ~/dev/workspace/qless/qless-asc/code/client/admin
+	#echo $linha
+	#linecount=$((linecount + 1))
+	#echo $linecount
+	arr+=($linha)
+#	grep -r -l -w "$linha"
+
+#	if grep -r -l -w "$linha"
+#	then
+#		echo "Entrou"
+#		cd ~/scripts
+#		echo $linha>>result.txt
+#	else
+#		echo "Foi nao"
+#	fi
+#done
+done < arq.txt
+}
+
+roda(){
+	for i in `echo ${arr[*]}`; do
+		cd ~/dev/workspace/qless/qless-asc/code/client/admin
+		grep -r -l -w $i
+
+		if [ ! $? -eq 0 ]
+		then
+			cd ~/scripts
+			echo $i>>result.txt
+		fi 
+	done 
+}
+
+search
+#sleep 2
+roda
